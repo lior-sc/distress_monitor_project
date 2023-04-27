5ffee50bfaead1024f311a8e179264c6f0993634
lior-sc <lior@soltrex.io>
2023-04-27 23:47:54 +0300

    initial commit | libraries installed and compiled

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+#include <Arduino.h>
+#include <Wire.h>
+#include <Adafruit_I2CDevice.h>
+#include <SPI.h>
+
+void setup()
+{
+  // put your setup code here, to run once:
+}
+
+void loop()
+{
+  // put your main code here, to run repeatedly:
+}
