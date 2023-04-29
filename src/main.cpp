c0b4d9b831240c7c63f38ad0221b183e1d6ec162
lior-sc <lior@soltrex.io>
2023-04-30 00:00:17 +0300

    TFT screen is setup and working
    NOTES
    * Some code jibbrish is there for the Wifi setup. the wifi is not
    working yet

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,29 @@
 #include <Arduino.h>
 #include <Wire.h>
 #include <Adafruit_I2CDevice.h>
+#include <Adafruit_ADXL345_U.h>
+#include <Adafruit_ST7735.h>
 #include <SPI.h>
 #include <ESP8266WiFi.h>
 #include <ESP8266HTTPClient.h>
 #include <WiFiClient.h>
+#include <TFTv2.h>
 
 #define BPM_SENSOR_PIN PIN_A0
+#define TFT_CS 15 // ESP8266 GPIO NUMBER
+#define TFT_RST 0 // ESP8266 GPIO NUMBER
+#define TFT_DC 2  // ESP8266 GPIO NUMBER
+
 #define BPM_HIGH_PULSE_READING 600
 
-///////////////////// Global variables /////////////////////
+////////////////////////////// Global variables //////////////////////////////
+
+// TFT variables
+Adafruit_ST7735 tft = Adafruit_ST7735(TFT_CS, TFT_DC, TFT_RST);
 
 // wifi variables
 const char *ssid = "liornet_WR";
-const char *ssid = "0544988409";
+const char *password = "0544988409";
 
 String server_name = "https://api.callmebot.com/whatsapp.php?phone=0528854006&text=Here%27s%20the%20location%20you%20requested:%20https://www.google.com/maps/search/?api=1%26query=32.166206415848514,34.89679626029493&apikey=7335050";
 
@@ -22,21 +32,23 @@ bool peak_detected = false;
 int heart_rate = 60;
 unsigned long int last_peak_time = false;
 
-// Function prototypes
+////////////////////////////// Function prototypes //////////////////////////////
 bool calculate_heart_rate(int, int, int);
+void TFT_setup(void);
 
 void setup()
 {
   // put your setup code here, to run once:
   pinMode(LED_BUILTIN, OUTPUT);
   Serial.begin(9600);
+  TFT_setup();
 }
 
 void loop()
 {
   // Serial.println(analogRead(PIN_A0));
   // Serial.println("\t");
-  calculate_heart_rate(BPM_SENSOR_PIN, 180, 50);
+  // calculate_heart_rate(BPM_SENSOR_PIN, 180, 50);
   // put your main code here, to run repeatedly:
 }
 
@@ -77,4 +89,23 @@ bool calculate_heart_rate(int signal_pin, int max_heart_rate, int min_heart_rate
   }
 
   return success;
+}
+
+void setup_wifi_connection(void)
+{
+}
+
+void TFT_setup(void)
+{
+  tft.initR(INITR_GREENTAB);
+  tft.fillScreen(ST77XX_BLACK);
+
+  tft.setCursor(0, 0);
+  tft.setTextColor(ST7735_GREEN);
+  tft.setTextWrap(true);
+  tft.setTextSize(2);
+  tft.println("Hello !");
+  tft.setTextColor(ST7735_CYAN);
+  tft.setTextSize(3);
+  tft.println("Hello !");
 }
