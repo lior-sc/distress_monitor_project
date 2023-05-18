ee92155dd29d1733edf8f10c5ba0a1fecf33dc36
lior-sc <lior@soltrex.io>
2023-05-18 20:16:52 +0300

    Create project

---
+++ main_software/src/main.cpp
@@ -0,0 +1,29 @@
+#include <Arduino.h>
+
+// adxl345 libs
+#include <Adafruit_I2CDevice.h>
+#include <Adafruit_Sensor.h>
+#include <Wire.h>
+#include <SPI.h>
+
+//
+
+// put function declarations here:
+int myFunction(int, int);
+
+void setup()
+{
+  // put your setup code here, to run once:
+  int result = myFunction(2, 3);
+}
+
+void loop()
+{
+  // put your main code here, to run repeatedly:
+}
+
+// put function definitions here:
+int myFunction(int x, int y)
+{
+  return x + y;
+}
