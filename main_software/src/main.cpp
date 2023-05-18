15a1c95d0d2e54dc1ee622134197bf033af109b3
lior-sc <lior@soltrex.io>
2023-05-18 20:32:01 +0300

    add ST7735 libraries
    NOTES
    * Everything compiles

--- main_software/src/main.cpp
+++ main_software/src/main.cpp
@@ -4,6 +4,9 @@
 #include <Adafruit_I2CDevice.h>
 #include <Adafruit_Sensor.h>
 #include <Wire.h>
+
+// ST7735 tft lcd libs
+#include <Adafruit_ST7735.h>
 #include <SPI.h>
 
 // Neo-6m libs
@@ -12,6 +15,20 @@
 // GPS defines
 #define GPS_RX_PIN 32
 #define GPS_TX_PIN 33
+
+// ST7735 defines
+#define TFT_CS 4   // Marked as pin A5 on the board
+#define TFT_RST 26 // Marked as pin A0 on the board
+#define TFT_A0 13  // Marked as pin A12 on the board
+#define MOSI_PIN 18
+#define MISO_PIN 19
+#define SCK_PIN 5
+
+// ST7735 variables
+Adafruit_ST7735 tft = Adafruit_ST7735(TFT_CS, TFT_A0, TFT_RST);
+
+// Neo-6m variables
+SoftwareSerial gpsSerial(GPS_RX_PIN, GPS_TX_PIN);
 
 // put function declarations here:
 int myFunction(int, int);
