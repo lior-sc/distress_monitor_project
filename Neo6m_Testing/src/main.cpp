35a5ad0b8aabe9bbd770e5f613deeacd2e823cc6
lior-sc <lior@soltrex.io>
2023-05-18 17:36:59 +0300

    gps tested and working

--- Neo6m_Testing/src/main.cpp
+++ Neo6m_Testing/src/main.cpp
@@ -1,18 +1,48 @@
 #include <Arduino.h>
+#include <SoftwareSerial.h>
+
+#define GPS_RX_PIN 32
+#define GPS_TX_PIN 33
 
 // put function declarations here:
 int myFunction(int, int);
 
-void setup() {
+SoftwareSerial gpsSerial(GPIO_NUM_32, GPIO_NUM_33);
+// SoftwareSerial sserial(rx, tx);
+int counter = 0;
+
+void setup()
+{
   // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(9600);
+  gpsSerial.begin(9600);
+  pinMode(13, OUTPUT);
+
+  for (int i = 0; i < 20; i++)
+  {
+    digitalWrite(13, !digitalRead(13));
+    delay(100);
+  }
 }
 
-void loop() {
+void loop()
+{
+
+  while (gpsSerial.available())
+  {
+    Serial.print((char)gpsSerial.read());
+  }
+  Serial.print("   done!  " + String(counter++));
+
+  delay(1000);
+  // gpsSerial.print("hello from gps");
+  digitalWrite(13, !digitalRead(13));
+  Serial.println();
   // put your main code here, to run repeatedly:
 }
 
 // put function definitions here:
-int myFunction(int x, int y) {
+int myFunction(int x, int y)
+{
   return x + y;
 }
