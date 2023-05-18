bbb2105bfe7555c807498e038fa4f84ca3d87881
lior-sc <lior@soltrex.io>
2023-05-18 20:54:37 +0300

    Add Wifi libs
    NOTES:
    * added various defines
    * added a callmebot related function

--- main_software/src/main.cpp
+++ main_software/src/main.cpp
@@ -13,17 +13,32 @@
 // Neo-6m libs
 #include <SoftwareSerial.h>
 
-// GPS defines
+// Wifi libs
+#include <UrlEncode.h>
+#include <HTTPClient.h>
+
+////////////// GPS defines /////////////
 #define GPS_RX_PIN 32
 #define GPS_TX_PIN 33
 
-// ST7735 defines
+////////////// ST7735 defines //////////////
 #define TFT_CS 4   // Marked as pin A5 on the board
 #define TFT_RST 26 // Marked as pin A0 on the board
 #define TFT_A0 13  // Marked as pin A12 on the board
 #define MOSI_PIN 18
 #define MISO_PIN 19
 #define SCK_PIN 5
+
+////////////// Wifi variables //////////////
+
+// Wifi address
+const char *ssid = "Galaxy A53 5G 4C72";
+const char *password = "otkm7250";
+
+/////////// callmebot variables ///////////
+String phoneNumber = "+972528854006";
+String apiKey = "7335050";
+String messsage = "Here's the location you requested: https://www.google.com/maps/search/?api=1&query=32.095368,34.769905";
 
 // ST7735 variables
 Adafruit_ST7735 tft = Adafruit_ST7735(TFT_CS, TFT_A0, TFT_RST);
