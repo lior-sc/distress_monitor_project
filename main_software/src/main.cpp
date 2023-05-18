125dc79730988227c2024426824f5f850e33fe5f
lior-sc <lior@soltrex.io>
2023-05-18 21:57:54 +0300

    Main setup sequence works
    NOTES
    * acceleration notifications does not work

--- main_software/src/main.cpp
+++ main_software/src/main.cpp
@@ -37,7 +37,7 @@
 #define ACC_MIN_VALUE 4      // m/s^2
 
 ////////////// Heart rate sensors defines //////////////
-#define HR_SENSOR_PIN PIN_A0      // pin number
+#define HR_SENSOR_PIN 20          // pin number
 #define HR_HIGH_PULSE_READING 550 // 10 bit analog read value
 #define MAX_HEART_RATE 180        // bpm
 #define MIN_HEART_RATE 60         // bpm
@@ -65,24 +65,426 @@ double longitude = 34.769904;
 // ADXL345 variables
 Adafruit_ADXL345_Unified accel = Adafruit_ADXL345_Unified(12345);
 
-// put function declarations here:
-int myFunction(int, int);
+// Heart rate variables
+/////////// Heart rate sensor variables
+typedef struct
+{
+  int buffer[100];
+  int buffer_size;
+  int head;
+  int tail;
 
+} CIRCULAR_BUFFER;
+
+CIRCULAR_BUFFER hr_cb;
+
+bool peak_detected = false;
+int avg_heart_rate = 60;
+unsigned long int last_peak_time = 0;
+
+////////////////////////////// Function prototypes //////////////////////////////
+
+// setup functions
+inline void TFT_setup(void);
+inline bool accel_setup(void);
+inline bool wifi_setup(void);
+inline void gps_setup(void);
+
+// test functions
+inline void accel_test_loop(void);
+inline void accel_test_loop_serial(void);
+inline void heart_rate_test_loop(void);
+
+// main algorithm functions
+inline void main_operational_setup(void);
+inline void main_operational_loop(void);
+
+// misc functions
+int calculate_heart_rate(void);
+float accel_get_acceleration_norm(void);
+void accel_buffer_add_data(float);
+float accel_buffer_get_oldest_data(void);
+void get_gps_raw(void);
+bool get_gps_lat_long(void);
+char msg_buffer[100];
+bool send_location_msg();
+bool sendWhatsappMessage(String);
+void exception_handler(String);
+void tft_print_headline(String);
+void tft_print_ok(void);
+void add_to_buffer(CIRCULAR_BUFFER *cb, int value);
+int read_from_buffer(CIRCULAR_BUFFER *cb);
+void update_avg_heart_rate();
+void update_heart_rate();
+void HR_setup();
+
+////////////////////// main / loop //////////////////////
 void setup()
 {
   // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  main_operational_setup();
 }
 
 void loop()
 {
   // put your main code here, to run repeatedly:
+  main_operational_loop();
 }
 
-// put function definitions here:
-int myFunction(int x, int y)
+////////////////////// Functions //////////////////////
+
+// main algorithm
+inline void main_operational_setup(void)
 {
-  return x + y;
+  Serial.begin(9600);
+  HR_setup();
+  TFT_setup();
+  wifi_setup();
+  accel_setup();
+  gps_setup();
+  tft_print_ok();
+}
+
+inline void main_operational_loop(void)
+{
+  // get location
+  // get_gps_lat_long();
+
+  // read sensors
+  float acc_norm = accel_get_acceleration_norm();
+  // bool HR_calculation_success = calculate_heart_rate();
+  update_avg_heart_rate();
+
+  // check free fall condition
+  if (acc_norm < ACC_MIN_VALUE)
+  {
+    // fall detected
+    tft_print_headline("Fall detected!");
+    tft.setTextColor(ST7735_WHITE);
+    tft.setTextSize(1);
+    tft.printf("sending alarm and current \nlocation\n\n");
+    bool success = send_location_msg();
+
+    if (success == true)
+    {
+      tft.printf("distress message sent!");
+    }
+    else
+    {
+      tft.printf("you're on you own my man!");
+    }
+    delay(2000);
+    tft_print_ok();
+  }
+
+  // check hit condition
+  if (acc_norm > ACC_MAX_VALUE)
+  {
+    // hit detected
+    tft_print_headline("Hit detected!");
+    tft.setTextColor(ST7735_WHITE);
+    tft.setTextSize(1);
+    tft.printf("sending alarm and current \nlocation\n\n");
+    bool success = send_location_msg();
+
+    if (success == true)
+    {
+      tft.printf("distress message sent!");
+    }
+    else
+    {
+      tft.printf("you can do it!");
+    }
+    delay(2000);
+    tft_print_ok();
+  }
+
+  // check heart rate condition
+  if (avg_heart_rate > MAX_HEART_RATE)
+  {
+    // send alarm
+    tft_print_headline("High HR!");
+    tft.setTextColor(ST7735_WHITE);
+    tft.setTextSize(1);
+    tft.printf("sending alarm and current \nlocation\n\n");
+    bool success = send_location_msg();
+
+    if (success == true)
+    {
+      tft.printf("distress message sent!");
+    }
+    else
+    {
+      tft.printf("too bad!");
+    }
+    delay(2000);
+    tft_print_ok();
+  }
+
+  // if (digitalRead(9) == HIGH)
+  // {
+  //   // https://randomnerdtutorials.com/esp8266-pinout-reference-gpios/
+  //   // need to pull this pin low with 10K resistor and attach to high
+
+  //   // send alarm
+  // }
+  delay(10);
+}
+
+// Heart rate sensor
+void HR_setup()
+{
+  // setup analog pin
+  pinMode(HR_SENSOR_PIN, INPUT); // its not harmful although its unnecessary
+
+  // set circular buffer size (<100)
+  hr_cb.buffer_size = HR_BUFFER_SIZE; // size of the buffer we intend to utilize
+
+  // put one value in buffer
+  int heart_rate = calculate_heart_rate();
+  add_to_buffer(&hr_cb, heart_rate);
+}
+
+int calculate_heart_rate()
+{
+  int sensor_value = analogRead(HR_SENSOR_PIN);
+  int threshold = HR_HIGH_PULSE_READING;
+  int measured_heart_rate = 0;
+  int heart_rate = 0;
+
+  if (sensor_value > threshold && !peak_detected)
+  {
+    /* A peak was detected. conduct calculations*/
+
+    // calculte measured heart rate and see that it fits withing specified boundaries
+    measured_heart_rate = 60000 / (int)(millis() - last_peak_time);
+
+    if (measured_heart_rate >= MIN_HEART_RATE && measured_heart_rate <= MAX_HEART_RATE)
+    {
+      heart_rate = measured_heart_rate;
+      // success = true;
+    }
+    else
+    {
+      // do nothing. heart_rate stays at -1
+    }
+
+    // mark peak detection and reset last peak time measurement
+    peak_detected = true;
+    last_peak_time = millis();
+  }
+  else if (sensor_value < threshold && peak_detected)
+  {
+    /* sensor reading left peak values. reset peak detection*/
+    peak_detected = false;
+  }
+
+  return heart_rate;
+}
+
+void update_heart_rate()
+{
+  int current_heart_rate = calculate_heart_rate();
+  add_to_buffer(&hr_cb, current_heart_rate);
+
+  return;
+}
+void update_avg_heart_rate()
+{
+  update_heart_rate();
+
+  int sum = 0;
+  for (int i = 0; i < HR_BUFFER_SIZE; i++)
+  {
+    sum += calculate_heart_rate();
+  }
+  avg_heart_rate = sum / HR_BUFFER_SIZE;
+}
+
+void add_to_buffer(CIRCULAR_BUFFER *cb, int value)
+{
+  cb->buffer[cb->head] = value;
+  cb->head = (cb->head + 1) % cb->buffer_size;
+  if (cb->head == cb->tail)
+  {
+    cb->tail = (cb->tail + 1) % cb->buffer_size; // discard oldest value
+  }
+}
+
+int read_from_buffer(CIRCULAR_BUFFER *cb)
+{
+  if (cb->head == cb->tail)
+  {
+    return -1; // buffer is empty
+  }
+  int value = cb->buffer[cb->tail];
+  cb->tail = (cb->tail + 1) % cb->buffer_size;
+  return value;
+}
+
+// Wifi
+inline bool wifi_setup(void)
+{
+  // establishing wifi connection
+  WiFi.begin(ssid, password);
+  tft.fillScreen(ST7735_BLACK);
+  tft.setTextSize(2);
+  tft.setTextColor(ST7735_MAGENTA);
+  tft.setCursor(0, 0);
+  tft.print("WiFi setup\n\n");
+
+  tft.setTextSize(1);
+  tft.setTextColor(ST7735_WHITE);
+  tft.printf("Connecting to: \n\n%s", ssid);
+
+  int count = 0;
+
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(1000);
+    tft.print(".");
+    count++;
+    if (count >= 20)
+    {
+      exception_handler("can't connect to Wifi");
+    }
+  }
+  tft.println("\n\nWiFi connected ");
+  tft.print("Connected to WiFi network with IP address: \n");
+  tft.println(WiFi.localIP());
+  tft.println();
+
+  delay(1000);
+
+  return true;
+}
+
+bool send_location_msg()
+{
+  bool success = false;
+  // tft.fillScreen(ST7735_BLACK);
+  // tft.setCursor(0, 0);
+  // tft.setTextColor(ST7735_WHITE);
+  // tft.setTextSize(1);
+  // tft.printf("Trying to send distress signal");
+
+  char msg_buffer[100];
+  sprintf(msg_buffer, "Help! here's my location: https://www.google.com/maps/search/?api=1&query=%f,%f", lattitude, longitude);
+
+  success = sendWhatsappMessage(String(msg_buffer));
+
+  if (success)
+  {
+    tft.fillScreen(ST7735_BLACK);
+    tft.setCursor(0, 0);
+    tft.setTextColor(ST7735_WHITE);
+    tft.setTextSize(2);
+    tft.printf("message sent!\n");
+    tft.setTextSize(1);
+    tft.printf("lat: %f\nlong: %f", lattitude, longitude);
+    Serial.printf("lat: %f\nlong: %f", lattitude, longitude);
+  }
+  while (gpsSerial.available())
+  {
+    int tmp = gpsSerial.read();
+    delay(GPS_SERIAL_READ_DELAY);
+  }
+  delay(1000);
+
+  return success;
+}
+
+// lcd
+inline void TFT_setup(void)
+{
+  SPI.begin();
+  tft.initR(INITR_GREENTAB);
+  // set
+  tft.fillScreen(ST77XX_BLACK);
+  tft.setRotation(1);
+  tft.setTextWrap(true);
+
+  tft.setTextSize(4);
+  tft.setTextColor(ST7735_ORANGE);
+  tft.setCursor(0, 0);
+  tft.println("Hello!");
+
+  tft.setTextSize(1);
+  tft.setTextColor(ST7735_WHITE);
+  tft.print("\nWhen life gets you down, \njust press the ");
+  tft.setTextColor(ST7735_GREEN);
+  tft.print("yellow \nbutton!\n\n");
+  tft.setTextColor(ST7735_WHITE);
+
+  delay(1000);
+  tft.print("\n\n  (It doesn't work!!)");
+  delay(250);
+}
+
+void tft_print_headline(String headline)
+{
+  tft.fillScreen(ST7735_BLACK);
+  tft.setTextSize(2);
+  tft.setTextColor(ST7735_MAGENTA);
+  tft.setCursor(0, 0);
+  tft.printf("%s\n\n", headline.c_str());
+}
+
+void tft_print_ok(void)
+{
+  tft.fillScreen(ST7735_BLACK);
+  tft.setCursor(0, 0);
+
+  tft.setTextColor(ST7735_GREEN);
+  tft.setTextSize(2);
+  tft.printf("Status: OK\n\n");
+
+  return;
+}
+
+// accelerometer
+inline bool accel_setup()
+{
+  Wire.begin();
+  // begin accelerometer operation
+  bool success = accel.begin();
+  // set range to +-16g (+-156.9 m/s)
+  accel.setRange(ADXL345_RANGE_16_G);
+  // get and print sensor data on serial
+  sensor_t sensor_data;
+  accel.getSensor(&sensor_data);
+
+  // Serial print the accelerometer data
+  Serial.println("\n////////////////////// adxl345 sensor data //////////////////////");
+  Serial.printf("Sensor id: %d", sensor_data.sensor_id);
+  Serial.printf("Resolution: %.3f [m/s^2]\n", sensor_data.resolution);
+  Serial.printf("max_value: %.2f [m/s^2]\n", sensor_data.max_value);
+  Serial.printf("min_value: %.2f [m/s^2]\n", sensor_data.min_value);
+
+  // print to tft screen
+  tft_print_headline("ADXL345");
+  tft.setTextColor(ST7735_WHITE);
+  tft.setTextSize(1);
+  tft.printf("Sensor id: %d\n", sensor_data.sensor_id);
+  tft.printf("Resolution: %.3f [m/s^2]\n", sensor_data.resolution);
+  tft.printf("max_value: %.2f [m/s^2]\n", sensor_data.max_value);
+  tft.printf("min_value: %.2f [m/s^2]\n", sensor_data.min_value);
+
+  // set delay to see the data
+  delay(1000);
+
+  // return the success of the accel.begin method
+  return success;
+}
+
+float accel_get_acceleration_norm()
+{
+  // get accelerometer data
+  sensors_event_t event;
+  accel.getEvent(&event);
+
+  // calculate and return acceleration vector norm
+  return (sqrt(pow(event.acceleration.x, 2) + pow(event.acceleration.y, 2) + pow(event.acceleration.z, 2)) - 2);
 }
 
 bool sendWhatsappMessage(String message)
@@ -117,37 +519,183 @@ bool sendWhatsappMessage(String message)
   return success;
 }
 
-bool send_location_msg()
+// GPS
+void gps_setup()
 {
-  bool success = false;
-  // tft.fillScreen(ST7735_BLACK);
-  // tft.setCursor(0, 0);
-  // tft.setTextColor(ST7735_WHITE);
-  // tft.setTextSize(1);
-  // tft.printf("Trying to send distress signal");
-
-  char msg_buffer[100];
-  sprintf(msg_buffer, "Help! here's my location: https://www.google.com/maps/search/?api=1&query=%f,%f", lattitude, longitude);
-
-  success = sendWhatsappMessage(String(msg_buffer));
-
-  // if (success)
-  // {
-  //   tft.fillScreen(ST7735_BLACK);
-  //   tft.setCursor(0, 0);
-  //   tft.setTextColor(ST7735_WHITE);
-  //   tft.setTextSize(2);
-  //   tft.printf("message sent!\n");
-  //   tft.setTextSize(1);
-  //   tft.printf("lat: %f\nlong: %f", lattitude, longitude);
-  //   Serial.printf("lat: %f\nlong: %f", lattitude, longitude);
-  // }
+  Serial.println("////////////////////// NEO-6M GPS setup //////////////////////");
+  Serial.println("Initiate softwareSerial on pins " + String(GPS_RX_PIN) + " (RX)  and pin" + String(GPS_TX_PIN) + " (TX)");
+  gpsSerial.begin(9600);
+  delay(1000);
   while (gpsSerial.available())
   {
-    int tmp = gpsSerial.read();
+    Serial.print(gpsSerial.read());
+    delay(1);
+  }
+
+  Serial.println("\n\nFinished GPS setup");
+
+  // write to TFT screen
+  tft_print_headline("Neo-6m");
+  delay(500);
+  tft.setTextColor(ST7735_WHITE);
+  tft.setTextSize(1);
+  tft.printf("Finished GPS setup\n\n");
+  tft.printf("getting current location");
+
+  for (int i = 0; i < 5; i++)
+  {
+    get_gps_lat_long();
+    delay(1000);
+    tft.print(".");
+  }
+
+  tft.printf("\n\nlat: %.6f \nlong: %.6f\n", lattitude, longitude);
+  delay(1000);
+
+  for (int i = 0; i < 3; i++)
+  {
+    tft.print(".");
+    if (send_location_msg())
+    {
+      break;
+    }
+    delay(2000);
+  }
+}
+
+void get_gps_raw()
+{
+  if (!gpsSerial.available())
+  {
+    return;
+    // Serial.println("no gps data. waiting");
+    // delay(10);
+  }
+  Serial.println();
+
+  while (gpsSerial.available())
+  {
+    Serial.print(gpsSerial.read());
     delay(GPS_SERIAL_READ_DELAY);
   }
-  // delay(1000);
 
+  Serial.println("\n");
+}
+
+bool get_gps_lat_long()
+{
+  bool success = false;
+
+  if (gpsSerial.available() > 0)
+  {
+    while (gpsSerial.available() > 0)
+    {
+      char temp = (char)gpsSerial.read();
+      gps.encode(temp);
+      delay(2);
+    }
+  }
+
+  if (gps.location.isValid())
+  {
+    lattitude = gps.location.lat();
+    longitude = gps.location.lng();
+
+    success = true;
+  }
   return success;
+}
+
+// test functions
+inline void accel_test_loop()
+{
+  // get adxl345 reading
+  sensors_event_t event;
+  accel.getEvent(&event);
+
+  // set TFT text configurations and clear existing text
+  tft.setCursor(0, 0);
+  tft.setTextColor(ST7735_MAGENTA);
+  tft.setTextWrap(true);
+  tft.setTextSize(2);
+  tft.fillRect(36, 0, 100, 52, ST7735_BLACK);
+  // print acceleration values
+  tft.println("x: " + String(event.acceleration.x));
+  tft.println("y: " + String(event.acceleration.y));
+  tft.println("z: " + String(event.acceleration.z));
+  tft.print("");
+
+  /** @note
+   * in order to avoid screen blinking we need to rewrite the number only if it is changed.
+   * we need to do a comparison
+   * */
+
+  delay(10);
+}
+
+inline void accel_test_loop_serial()
+{
+  // get adxl345 reading
+  sensors_event_t event;
+  accel.getEvent(&event);
+
+  // print acceleration values
+  printf("x: %.3f, y: %.3f, z: %.3f, norm: %.3f\n",
+         event.acceleration.x,
+         event.acceleration.y,
+         event.acceleration.z,
+         accel_get_acceleration_norm());
+
+  /** @note
+   * in order to avoid screen blinking we need to rewrite the number only if it is changed.
+   * we need to do a comparison
+   * */
+
+  delay(10);
+}
+
+inline void heart_rate_test_loop()
+{
+  Serial.println(analogRead(HR_SENSOR_PIN));
+}
+
+inline void gps_test_loop()
+{
+  while (!get_gps_lat_long())
+  {
+    // do nothing
+  }
+  tft.fillScreen(ST7735_BLACK);
+  tft.setTextSize(1);
+  tft.setCursor(0, 0);
+  tft.setTextColor(ST7735_MAGENTA);
+  tft.printf("lat: %.13f \n", lattitude);
+  tft.setTextColor(ST7735_ORANGE);
+  tft.printf("long: %.13f \n", longitude);
+  char str[100];
+  sprintf(str, "lat: %.16f  long: %.16f \n", lattitude, longitude);
+  Serial.printf("%s\n", str);
+  // Serial.printf("lat: %.16f  long: %.16f \n", lattitude, longitude);
+  delay(1000);
+  return;
+}
+
+void exception_handler(String msg)
+{
+  tft.fillScreen(ST7735_BLACK);
+  tft.setTextColor(ST7735_RED);
+  tft.setTextSize(2);
+  tft.setCursor(0, 0);
+  tft.printf("Exception!\n");
+  tft.setTextColor(ST7735_WHITE);
+  tft.setTextSize(1);
+  tft.printf("%s\n", msg.c_str());
+  tft.setTextColor(ST7735_GREEN);
+  tft.printf("\n\nreset controller to \ncontinue!");
+  delay(20000);
+
+  while (1)
+  {
+    // do nothing
+  }
 }
